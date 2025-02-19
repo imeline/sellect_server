d6d264d5d4d7ec6f6793bf21b7852d4a88b94c65
Sangkwon Lee <sangkwon26@gmail.com>
2025-02-19 09:49:34 +0900

    feat: 검색 자동완성을 위한 배치 작업 구현
    - 배치 시간 기준 오전 2:00

--- src/main/java/com/sellect/server/search/repository/SearchLogRepository.java
+++ src/main/java/com/sellect/server/search/repository/SearchLogRepository.java
@@ -1,9 +1,13 @@
 package com.sellect.server.search.repository;
 
 import com.sellect.server.search.domain.SearchLog;
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface SearchLogRepository {
 
     SearchLog save(SearchLog searchLog);
+
+    List<Object[]> findSearchLogsForBatch(LocalDateTime startTime, LocalDateTime endTime);
 
 }
