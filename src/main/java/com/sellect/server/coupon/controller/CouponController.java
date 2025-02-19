f560754c64aa15fb25af3465d1fe02351d8a5e46
kongji <kongji4092@gmail.com>
2025-02-19 16:36:26 +0900

    refactor: 엔드포인트 prefix 통일

--- src/main/java/com/sellect/server/coupon/controller/CouponController.java
+++ src/main/java/com/sellect/server/coupon/controller/CouponController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/coupon")
+@RequestMapping("/api/v1/coupon")
 @RequiredArgsConstructor
 public class CouponController {
 
