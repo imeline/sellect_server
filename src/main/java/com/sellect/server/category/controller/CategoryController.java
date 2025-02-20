54d484dd7ed9f934ec86fc8818be0196c2b5da1b
Sangkwon Lee <sangkwon26@gmail.com>
2025-02-20 19:20:45 +0900

    modify: ApiResponse로 카테고리 조회 랩핑 후 응답하도록 수정

--- src/main/java/com/sellect/server/category/controller/CategoryController.java
+++ src/main/java/com/sellect/server/category/controller/CategoryController.java
@@ -2,9 +2,9 @@ package com.sellect.server.category.controller;
 
 import com.sellect.server.category.application.CategoryService;
 import com.sellect.server.category.controller.request.CategoryReadResponse;
+import com.sellect.server.common.response.ApiResponse;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,8 +18,8 @@ public class CategoryController {
     private final CategoryService categoryService;
 
     @GetMapping("/categories")
-    public ResponseEntity<List<CategoryReadResponse>> readCategories() {
+    public ApiResponse<List<CategoryReadResponse>> readCategories() {
         List<CategoryReadResponse> result = categoryService.readCategories();
-        return ResponseEntity.ok(result);
+        return ApiResponse.ok(result);
     }
 }
