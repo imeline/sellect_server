6bf3cf0714ea6a10ca4b70f9b23773cd1dc05ef3
Sangkwon Lee <sangkwon26@gmail.com>
2025-02-21 19:50:05 +0900

    style: OrderAddRequest의 todo 제거

--- src/main/java/com/sellect/server/order/controller/request/OrderAddRequest.java
+++ src/main/java/com/sellect/server/order/controller/request/OrderAddRequest.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 public record OrderAddRequest(
 
-    // todo: 총 가격이 String 인 이유 체크
     @Positive(message = "가격은 1원 이상이어야 합니다.")
     @Pattern(regexp = "^[0-9]+$", message = "가격은 정수만 입력 가능합니다.")
     String totalPrice,
