bd4cf1a5c7c0ade8ee140731ee9c42507bb79d44
kongji <kongji4092@gmail.com>
2025-02-24 16:22:28 +0900

    refactor: 개선된 결제 초기화 응답 및 결제 승인 로직 정리

--- src/main/java/com/sellect/server/payment/controller/PaymentController.java
+++ src/main/java/com/sellect/server/payment/controller/PaymentController.java
@@ -10,8 +10,6 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,18 +33,17 @@ public class PaymentController {
         @RequestParam(value = "size", defaultValue = "5") int size) {
         List<PaymentHistoryResponse> paymentHistory = paymentService.getPaymentHistory(user, page,
             size);
-
         return ApiResponse.ok(paymentHistory);
     }
 
     @PostMapping("/ready")
-    public ResponseEntity<?> initialPayment(@AuthUser User user,
+    public ApiResponse<String> initialPayment(@AuthUser User user,
         @RequestBody PaymentRequest request) {
         String redirectUrl = paymentService.initialPayment(user, request);
         HttpHeaders headers = new HttpHeaders();
         headers.add("Location", redirectUrl);
         log.info(redirectUrl);
-        return new ResponseEntity<>(headers, HttpStatus.FOUND);
+        return ApiResponse.ok(redirectUrl);
     }
 
     @GetMapping("/success/{pid}")
