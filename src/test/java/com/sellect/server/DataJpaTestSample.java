a9cb6abfb228f19f212f60a99230e3bcd476177f
Sangkwon Lee <sangkwon26@gmail.com>
2025-03-04 21:49:26 +0900

    test: DataJpaTestSample 코드 작성

---
+++ src/test/java/com/sellect/server/DataJpaTestSample.java
@@ -0,0 +1,20 @@
+package com.sellect.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.transaction.annotation.Transactional;
+
+@DataJpaTest
+@Transactional
+public class DataJpaTestSample {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Test
+    void contextLoads() {
+    }
+
+}
